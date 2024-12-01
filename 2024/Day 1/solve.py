cdaa52bda7e474d5616dd90f421f4ff83044dc99
Andreas Fjäder <andreas.fjader@quinyx.com>
2024-12-02 00:36:32 +0100

    Solution day 1

---
+++ 2024/Day 1/solve.py
@@ -0,0 +1,12 @@
+# Advent of Code
+
+file = 'in'
+delimiter = '   '
+
+input = open(f'{file}.txt', 'r').read().splitlines()
+
+nrs1 = sorted(int(x.split(delimiter)[0]) for x in input)
+nrs2 = sorted(int(x.split(delimiter)[1]) for x in input)
+
+print('Part 1: ', sum([abs(y - x) for x, y in zip(nrs1, nrs2)]))
+print('Part 2: ', sum([(x * nrs2.count(x)) for x in nrs1]))
---
+++ aoc.py
@@ -0,0 +1,143 @@
+import os
+import random
+import argparse
+import subprocess
+
+#########################
+# Configs and variables #
+#########################
+
+LANGUAGES = ['python', 'node.js', 'c#', 'java']
+LANG_EXTENSIONS = { 'python': 'py', 'node.js': 'js', 'c#': 'cs', 'java': 'java' }
+ROOT_DIR = 'D:\\Advent of Code'
+SRC_FILE_NAME = 'solve'
+
+###################
+# Language setups #
+###################
+
+def setup_python(day_dir):
+    file = setup_language_file(day_dir, 'py')
+    with open(file, 'w') as f:
+        f.write('# Advent of Code\n\n')
+
+def setup_nodejs(day_dir):
+    file = setup_language_file(day_dir, 'js')
+    return
+
+def setup_csharp(day_dir):
+    file = setup_language_file(day_dir, 'cs')
+    return
+
+def setup_java(day_dir):
+    file = setup_language_file(day_dir, 'java')
+    return
+
+def setup_language_file(day_dir, lang_ext):
+    return f'{day_dir}\\{SRC_FILE_NAME}.{lang_ext}'
+
+
+###################
+# Directory setup #
+###################
+
+def setup_dir(year, day, language):
+    day_dir = os.path.join(ROOT_DIR, str(year), f'Day {day}')
+    if os.path.exists(day_dir):
+        print(f'Found folder for Year {year} Day {day}. Opening in VS Code...')
+        return day_dir
+    
+    os.makedirs(day_dir)
+    create_input_file(day_dir)
+    
+    # Setup language specific files
+    if language.lower() == 'python':
+        setup_python(day_dir)
+    elif language.lower() == 'node.js':
+        setup_nodejs(day_dir)
+    elif language.lower() == 'c#':
+        setup_csharp(day_dir)
+    elif language.lower() == 'java':
+        setup_java(day_dir)
+    
+    
+    return day_dir
+
+def create_input_file(day_dir):
+    input_file = f'{day_dir}\\input.txt'
+    if not os.path.exists(day_dir):
+        print('Error creating input file.')
+        exit()
+    
+    open(input_file, 'w').close()
+
+
+####################
+# Input validation #
+####################
+
+def validate_day(day):
+    if day < 1 or day > 25:
+        raise ValueError('Day must be between 1 and 25')
+
+def validate_language(language: str):
+    if language.lower() not in LANGUAGES and language.lower() not in LANG_EXTENSIONS.values():
+        raise ValueError(f'Invalid language: {language}')
+
+########################
+# Auto open in VS Code #
+########################
+
+def open_vscode(path):
+    try:
+        subprocess.run(['D:\\Microsoft VS Code\\bin\\code.cmd', path], check=True)
+    except subprocess.CalledProcessError:
+        print('Error opening VS Code')
+        exit()
+
+####################
+# Argument parsing #
+####################
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser(description='Advent of Code setup tool')
+    arg_parser.add_argument('-y', '--year', type=int, required=True, help='Year of the challenge (i.e. 2024)')
+    arg_parser.add_argument('-d', '--day', type=int, required=True, help='Day of the challenge (1-25)')
+    arg_parser.add_argument('-l', '--language', type=str, choices=LANGUAGES, help='Language to use (random if not specified)')
+    
+    args = arg_parser.parse_args()
+    validate_day(args.day)
+    validate_language(args.language)
+
+    return args.year, args.day, args.language
+
+###########################
+# Picking random language #
+###########################
+
+def pick_random_language():
+    return random.choice(LANGUAGES)
+
+########
+# Main #
+########
+
+def main():
+    year, day, language = parse_args()
+    if language is None:
+        language = pick_random_language()
+    
+    day_dir = setup_dir(year, day, language)
+    if day_dir is None:
+        print('Error setting up directory')
+        exit()
+    
+    open_vscode(day_dir)
+    print('Happy coding!')
+
+###############
+# Entry point #
+###############
+
+if __name__ == '__main__':
+    main()
