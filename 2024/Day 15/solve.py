1a8e7a927213e2a66488ee73a1634b62a218fbba
Andreas Fjäder <andreas.fjader@quinyx.com>
2025-01-21 07:59:49 +0100

    Solve day 15 p2

--- 2024/Day 15/solve.py
+++ 2024/Day 15/solve.py
@@ -1,4 +1,5 @@
 # Advent of Code Day 15
+from collections import deque
 
 def parse_input(input: str):
     grid, directions = input.split('\n\n')
@@ -12,16 +13,37 @@ def parse_input(input: str):
 
     return (grid, guard, "".join(directions.splitlines()))
 
+def parse_input_2(puzzle_input: str):
+    parsed_map = []
+    grid, directions = puzzle_input.split('\n\n')
+    grid = [list(g) for g in grid.splitlines()]
+    for line in grid:
+        s = ""
+        for c in line:
+            if c == '.': s += '..'
+            elif c == '#': s += '##'
+            elif c == 'O': s += '[]'
+            elif c == '@': s += '@.'
+        parsed_map.append(list(s))
+    
+    guard = None
+    for y in range(len(parsed_map)):
+        for x in range(len(parsed_map[0])):
+            if parsed_map[y][x] == '@':
+                guard = (x, y)
+                break
+    
+    return (parsed_map, guard, "".join(directions.splitlines()))
+
 def is_within_grid(grid: list[str], x: int, y: int) -> bool:
     return 0 <= x < len(grid[0]) and 0 <= y < len(grid)
 
-def get_gps_score(grid: list[str]):
+def get_gps_score(grid: list[str], c: str) -> int:
     count = 0
     for y in range(len(grid)):
         for x in range(len(grid[0])):
-            if grid[y][x] == 'O':
+            if grid[y][x] == c:
                 count += (100 * y + x)
-    
     return count
 
 def get_boxes_in_line(grid, x, y, dx, dy):
@@ -52,7 +74,6 @@ def part1(puzzle_input: str) -> int:
         (x, y) = guard
         dx = 1 if d == '>' else -1 if d == '<' else 0
         dy = 1 if d == 'v' else -1 if d == '^' else 0
-        # print(dx, dy)
         
         if not is_within_grid(grid, x+dx, y+dy) or grid[y+dy][x+dx] == '#':
             continue
@@ -61,12 +82,10 @@ def part1(puzzle_input: str) -> int:
             grid[y][x] = '.'
             grid[y+dy][x+dx] = '@'
             guard = (x+dx, y+dy)
-            # print_grid(grid)
             continue
 
         # Else we've hit a box.
         boxes = get_boxes_in_line(grid, x, y, dx, dy)
-        # print(boxes)
         if len(boxes) == 0:
             continue
 
@@ -76,12 +95,99 @@ def part1(puzzle_input: str) -> int:
         grid[y][x] = '.'
         grid[y+dy][x+dx] = '@'
         guard = (x+dx, y+dy)
-        # print_grid(grid)
-        # print('\n')
 
+    return get_gps_score(grid, 'O')
 
-    return get_gps_score(grid)
+def part2(puzzle_input: str) -> int:
+    grid, guard, directions = parse_input_2(puzzle_input)
+    for d in directions:
+        (x, y) = guard
+        dx = 1 if d == '>' else -1 if d == '<' else 0
+        dy = 1 if d == 'v' else -1 if d == '^' else 0
+        
+        nx, ny = x+dx, y+dy
+        next_grid_spot = grid[ny][nx]
+        if not is_within_grid(grid, nx, ny) or next_grid_spot == '#':
+            continue
+        
+        if next_grid_spot == '.':
+            grid[y][x] = '.'
+            grid[ny][nx] = '@'
+            guard = (nx, ny)
+            continue
+        
+        # Else we've hit [ or ].
+        # Moving sideways is similar to part 1
+        if dy == 0 and dx != 0:
+            simx = nx
+            moved_boxes_sim = []
+            while True:
+                c = grid[ny][simx]
+                if c in '[]':
+                    moved_boxes_sim.append((simx + dx, ny, c))
+                elif c == '#':
+                    moved_boxes_sim.clear()
+                    break
+                elif c == '.':
+                    break
+                simx += dx
+                
+            if len(moved_boxes_sim) == 0:
+                continue
+            
+            for bx, by, bc in moved_boxes_sim:
+                grid[by][bx] = bc
+            grid[ny][nx] = '@'
+            grid[y][x] = '.'
+            guard = (nx, ny)
+                
+        elif dy != 0 and dx == 0:
+            # Bit tricker, we need to check +-1 in the x direction to find the entire box
+            bx, by = nx, ny
+            q = deque([(bx, by)])
+            moved_boxes_sim = []
+            old_box_pos = []
+            seen = set()
+            while q:
+                bx, by = q.popleft()
+                if (bx, by) in seen or not is_within_grid(grid, bx, by):
+                    continue
+                seen.add((bx, by))
+                c = grid[by][bx]
+                moved_boxes_sim.append((bx, by+dy, c))
+                old_box_pos.append((bx, by))
+                
+                if c == '[':
+                    q.append((bx+1, by))
+                    nb = grid[by+dy][bx] # Check if there's a box in the delta Y position
+                    if nb in '[]':
+                        q.append((bx, by+dy))
+                    elif nb == '#':
+                        moved_boxes_sim.clear()
+                        break
+                elif c == ']':
+                    q.append((bx-1, by))
+                    nb = grid[by+dy][bx]
+                    if nb in '[]':
+                        q.append((bx, by+dy))
+                    elif nb == '#':
+                        moved_boxes_sim.clear()
+                        break
 
+            if len(moved_boxes_sim) == 0:
+                continue
+
+            for bx, by in old_box_pos:
+                grid[by][bx] = '.'
+
+            for bx, by, bc in moved_boxes_sim:
+                grid[by][bx] = bc
+
+            grid[ny][nx] = '@'
+            grid[y][x] = '.'
+            guard = (nx, ny)
+    return get_gps_score(grid, '[')
+    
 def print_grid(grid):
     for col in grid:
         r = []
@@ -96,4 +202,8 @@ if __name__ == "__main__":
     actual_input = open('input.txt').read()
 
     print('Part 1 (test):', part1(test_input))
-    print('Part 1 (test):', part1(actual_input))
+    print('Part 1 (actual):', part1(actual_input))
+    print('Part 2 (test):', part2(test_input))
+    print('Part 2 (actual):', part2(actual_input))
+    
+    
